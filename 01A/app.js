32fee3e5c344f42cc527acc1d23721d74f275ef8
Evert <37932@ma-web.nl>
2023-11-21 15:05:26 +0100

    Let's

--- 01A/app.js
+++ 01A/app.js
@@ -4,6 +4,20 @@ class App
     {
         console.log("hello world!");
         //code gaat hier!
+        let appNaam = ("String");
+        let versienummer = (1 + "," + 1);
+        let versiedatum = (21 + "-" + 11 + "-" + 2023);
+        let autheur = ("Everardus");
+        let copyright = ("The Council");
+        let distributeur = ("Enkhuizen");
+        let darkmode = (true);
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
     }
 }
 
