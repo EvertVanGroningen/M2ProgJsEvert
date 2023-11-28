fc7deef7797d6036f1dc9b89bf19995cc37c9de0
Evert <37932@ma-web.nl>
2023-11-28 15:23:17 +0100

    2

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -7,6 +7,11 @@ class App
         let canvas = document.getElementById("canvasId");
         console.log(canvas);
         let g = canvas.getContext("2d");
+        
+        g.fillStyle = "#660066";
+        g.fillRect(0,0,canvas.width,canvas.height);
+
+        g.fillStyle = "#ffff00";
         g.fillRect(0,0,10,10);
     }
 }
